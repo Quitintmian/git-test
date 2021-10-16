9a126eb2b327bf982243f33052f2fde9c039844a
lupetra <gyq@localgit.com>
2021-10-16 19:44:25 +0800

    added hashmap

--- src/main/java/com/gyq/git/GitTest.java
+++ src/main/java/com/gyq/git/GitTest.java
@@ -1,5 +1,7 @@
 package com.gyq.git;
 
+import java.util.HashMap;
+
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
@@ -7,5 +9,10 @@ public class GitTest {
         System.out.println("hello git 3!");
 
         System.out.println("测试pull test");
+        HashMap<Integer,String> map = new HashMap<>();
+        map.put(1,"one");
+        map.put(2,"two");
+
+        System.out.println(map.get(1));
     }
 }
